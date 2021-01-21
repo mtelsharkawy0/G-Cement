d917adfab7765fbae75bb24766cbb54577cd84c1
mtelsharkawy0 <mtelsharkawy0@gmail.com>
2021-01-21 21:42:19 +0200

    3

--- public/scr.js
+++ public/scr.js
@@ -1,6 +1,6 @@
 change=(page)=>{
     let article=document.getElementsByTagName("article")[0];
-    fetch(`http://localhost:80/api/${page}`)
+    fetch(`http://localhost:8080/api/${page}`)
     .then(response=>response.text())
     .then(response=>article.innerHTML=response)
 }
