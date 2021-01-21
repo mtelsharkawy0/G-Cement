ead853f9b1146e7cd5e29425cba88cb67cb31879
mtelsharkawy0 <mtelsharkawy0@gmail.com>
2021-01-22 00:10:54 +0200

    3

--- back.js
+++ back.js
@@ -4,7 +4,8 @@ const app = express();
 let port = process.env.PORT||8080
 app.use(express.static("public",{}))
 app.get("/api/:id",(req,res)=>{res.sendFile(path.join(__dirname,`${req.params.id}.html`))})
-app.get('*', function(req, res){
-    res.status(404).sendFile(path.join(__dirname,`404.html`))
-  });
+app.use(function(req,res){
+  res.status(404).sendFile(path.join(__dirname,`404.html`));
+});
+
 app.listen(port,()=>console.log("working"));
