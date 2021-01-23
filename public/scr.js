6aba7cf43ffaffd38b33fbe92ce5d772ddcd1ef4
mtelsharkawy0 <mtelsharkawy0@gmail.com>
2021-01-23 22:56:20 +0200

    3

--- public/scr.js
+++ public/scr.js
@@ -3,4 +3,30 @@ change=(page)=>{
     fetch(`./api/${page}`)
     .then(response=>response.text())
     .then(response=>article.innerHTML=response)
+    .catch((err)=>{return 0})
+}
+removezoom = ()=>{
+    let zoom=document.getElementsByClassName("zoom")[0];
+    zoom.innerHTML=""
+    zoom.style.display="none";
+}
+galleryzoom =(element)=>{
+    let zoom=document.getElementsByClassName("zoom")[0];
+    let src=element.attributes.src.nodeValue;
+
+    zoom.innerHTML= `<img
+    class="zoomimage" 
+    src=${src} 
+    style="
+        top:${document.getElementsByTagName("html")[0].scrollTop}px;
+        "
+    alt="">`;
+    zoom.style.display="block";
+    zoom.style.position="absolute";
+    zoom.style.height=`${document.documentElement.scrollHeight}px`;
+    zoom.style.width="99vw";
+    zoom.style.backgroundColor="rgb(0,0,0,0.5)";
+    zoom.style.zIndex="999";
+    zoom.style.left="0px";
+    zoom.style.top=`0px`;
 }
