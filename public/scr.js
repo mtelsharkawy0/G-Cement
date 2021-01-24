dd65b793ab942c659233a398964cb740616ca541
mtelsharkawy0 <mtelsharkawy0@gmail.com>
2021-01-24 04:13:22 +0200

    3

--- public/scr.js
+++ public/scr.js
@@ -1,9 +1,20 @@
+var lang="e"
 change=(page)=>{
     let article=document.getElementsByTagName("article")[0];
     fetch(`./api/${page}`)
     .then(response=>response.text())
     .then(response=>article.innerHTML=response)
+    .then(()=>newpagelang())
     .catch((err)=>{return 0})
+}
+newpagelang=()=>{
+    console.log("adsad");
+    if (lang == "e"){
+        english()
+    }
+    else{
+        arabic()
+    }
 }
 removezoom = ()=>{
     let zoom=document.getElementsByClassName("zoom")[0];
@@ -29,4 +40,35 @@ galleryzoom =(element)=>{
     zoom.style.zIndex="999";
     zoom.style.left="0px";
     zoom.style.top=`0px`;
+}
+english=()=>{
+    lang="e"
+    document.getElementById("arabic").style.color="white"
+    document.getElementById("arabic").style.backgroundColor="rgba(23, 61, 1, 0.384)"
+    document.getElementById("eng").style.color="goldenrod"
+    document.getElementById("eng").style.backgroundColor="rgba(52, 138, 3, 0.384)"
+    engl=Array.from(document.getElementsByClassName("eng"))
+    arab=Array.from(document.getElementsByClassName("arabic"))
+    engl.forEach(element => {
+        element.style.display="inherit"
+    });
+    arab.forEach(element => {
+        element.style.display="none"
+    });
+}
+arabic=()=>{
+    lang="a"
+    document.getElementById("arabic").style.color="goldenrod"
+    document.getElementById("arabic").style.backgroundColor="rgba(52, 138, 3, 0.384)"
+    document.getElementById("eng").style.color="white"
+    document.getElementById("eng").style.backgroundColor="rgba(23, 61, 1, 0.384)"
+    engl=Array.from(document.getElementsByClassName("eng"))
+    arab=Array.from(document.getElementsByClassName("arabic"))
+    engl.forEach(element => {
+        element.style.display="none"
+    });
+    arab.forEach(element => {
+        element.style.display="inherit"
+    });
+
 }
